0b04fc0795b3c392340e545bc8542a36481b4293
Patric Khiev <r@Rs-MacBook-Pro.local>
2018-09-05 20:20:24 -0500

    solved2probs

--- classes.js
+++ classes.js
@@ -13,7 +13,6 @@
 */
 
 ////////// PROBLEM 1 //////////
-
 /*
   Make a class to help us build all of the employees.
   Each employee has the following properties:
@@ -30,8 +29,17 @@
 */
 
 //Code Here
-
-
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -50,8 +58,25 @@
 */
 
 //Code Here
+class Manager {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+    this.reports = []
+  }
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+  hire(employee){
+    this.reports.push(employee)
+  }
 
-
+  fire(index){
+    this.reports.splice(index,1)
+  }
+}
 
 ////////// PROBLEM 3 //////////
 
