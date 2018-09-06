c3632f4c59ff6492a75fa85b295223cade1609db
Patric Khiev <r@Rs-MacBook-Pro.local>
2018-09-05 21:20:18 -0500

    solved2probs

--- nesting.js
+++ nesting.js
@@ -51,8 +51,19 @@ var employees = [
 */
 
 //Code Here
+function employeeUpdater(){
+  for(let i = 0; i < employees.length; i++){
+    if(employees[i].firstName === "Theo"){
+      employees.splice(i,1);
+    }
+    if (employees[i].firstName === "Lorie"){
+     employees[i].department = "HR";
+    }
+  }
+  return employees;
+} 
 
-
+employeeUpdater(employees);
 
 ////////// PROBLEM 2 //////////
 
@@ -69,7 +80,16 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
 */
 
 //Code Here
+function removeDuplicates(){
+  var newArray = [];
+  for(let i = 0; i < workplaceAccidents.length; i++){
+    if(newArray.includes(workplaceAccidents[i])===false){
+      newArray.push(workplaceAccidents[i]);
+    }
+}
+return newArray;
 
+}
 
 
 ////////// PROBLEM 3 //////////
